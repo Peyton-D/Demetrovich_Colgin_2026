7be634f881f501aca95054ee0b4ba847464ce36d
Peyton-D <148901494+Peyton-D@users.noreply.github.com>
2026-04-09 14:12:57 -0500

    Update pipeline.m

--- pipeline.m
+++ pipeline.m
@@ -2,20 +2,22 @@
 % follows the analyses in order to recreate the data figures from the paper
 
 % 1. read in data struct
-load('cellStruct1_14_26.mat')
+load('cellStruct1_14_26.mat');
 
 % 2. Figs. 2-4: Ratemaps, Spatial Correlation, and Rate Overlap
-place_cell_metrics(cellStruct)
+place_cell_metrics(cellStruct);
 
 % 3. Fig. 5: Rat exploration time on track
-exploration_metrics(cellStruct)
+exploration_metrics(cellStruct);
+
+% 3.5. Fig. S2-S3: Dentate spike detection and classification - REQUIRES ACCESS TO EACH
+% SESSION'S RECORDING DATA - WILL NOT RUN!
+evaluate_scoring.m; % find the best standard deviation threshold (4.0)
+ds_methods_figure.m; 
+rerun_ds_detection(cellStruct, 4);
 
 % 4. Figs. 6-9,S4: dentate spike rates and PETHs - REQUIRES ACCESS TO EACH
-% SESSION'S LFP DATA
-dentate_spike_metrics(cellStruct)
-
-%% optional / supplmental
-
-% 4. Figs. S2-S3: dentate spike detection and classification methods
+% SESSION'S RECORDING DATA - WILL NOT RUN!
+dentate_spike_metrics(cellStruct);
 
 
